810f76cfab91ece8c4b0c4cf110be3343e1b2bad
wanderingsunbi <neoguysm@gmail.com>
2025-11-29 14:39:02 +0900

    날짜 관련 유틸 추가

--- src/services/habit.service.js
+++ src/services/habit.service.js
@@ -1,10 +1,20 @@
 import * as habitRepo from "../repositories/habit.repository.js";
 
 // 날짜 관련 유틸
+// 주차 계산 함수
+function getWeekNumber(date = new Date()) { 
+  const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
+  const pastDays = Math.floor((date - firstDayOfYear) / 864e5);
+  return Math.floor((pastDays + firstDayOfYear.getDay() + 1) / 7) + 1;
+}
+
+// 날짜 시작 / 다음날 시작 구하기
 const startOfDay = (date) =>
-    new Date(date.getFullYear(), date.getMonth(), date.getDate());
+  new Date(date.getFullYear(), date.getMonth(), date.getDate());
+
 const startOfNextDay = (date) =>
-    new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1);
+  new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1);
+
 
 // 전체 목록 조회
 export async function getHabits(studyId) {
@@ -17,7 +27,7 @@ export async function createHabit(studyId, payload) {
 
   return habitRepo.createHabit({
     STUDY_ID: studyId,
-    WEEK_NUM: 0,
+    WEEK_NUM: getWeekNumber(now),
     NAME: name,
     MON: days.includes("MON"),
     TUE: days.includes("TUE"),
